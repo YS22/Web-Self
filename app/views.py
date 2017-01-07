8c8cb800997b24be2a2664ef0c61abcaffa2d7b4
YS22 <1774507011@qq.com>
2017-01-07 23:35:01 +0800

    yu

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template, flash, redirect, session, url_for, request, g
-from flask_login import login_user, logout_user, current_user, login_required
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid,models
 from forms import LoginForm
 from models import User
@@ -76,6 +76,8 @@ def after_login(resp):
 @app.before_request
 def before_request():
     g.user = current_user
+
+
 
 
 @app.route('/logout')
