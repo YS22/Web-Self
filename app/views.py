c54d1bb6a31b07c4bcbbe542eccfbe7d9530bc3c
YS22 <1774507011@qq.com>
2017-01-07 23:42:38 +0800

    yu

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template, flash, redirect, session, url_for, request, g
-from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid,models
 from forms import LoginForm
 from models import User
